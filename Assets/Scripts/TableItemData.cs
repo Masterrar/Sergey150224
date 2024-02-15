e3540bf0471107560fc70f0d0920bda64db53fd3
Рогов Артем Алексеевич <artem.rogov@trulysocialgames.com>
2024-02-15 16:12:54 +0300

    done

---
+++ Assets/Scripts/TableItemData.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class TableItemData
+{
+    [SerializeField] public int id { get; private set; }
+    [SerializeField] public string name { get; private set;  }
+
+    public TableItemData(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ Assets/Scripts/Utility/UnityEngineUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public static class UnityEngineUtils
+    {
+        public static void DestroyChilds(this GameObject container)
+        {
+            foreach (Transform child in container.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+    }
+}
